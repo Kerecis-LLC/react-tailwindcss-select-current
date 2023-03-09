1a883f11c50658e09dc703f5dba511a751c38fe2
Brian S Memmen <brian.memmen@gmail.com>
2023-03-09 16:29:33 -0500

    .

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -194,10 +194,6 @@ const CloseIcon = ({ className = "" }) => {
     return (require$$0.createElement("svg", { className: className, fill: "currentColor", viewBox: "0 0 20 20", xmlns: "http://www.w3.org/2000/svg" },
         require$$0.createElement("path", { fillRule: "evenodd", d: "M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z", clipRule: "evenodd" })));
 };
-const SearchIcon = ({ className = "" }) => {
-    return (require$$0.createElement("svg", { className: className, fill: "none", stroke: "currentColor", viewBox: "0 0 24 24", xmlns: "http://www.w3.org/2000/svg" },
-        require$$0.createElement("path", { strokeLinecap: "round", strokeLinejoin: "round", strokeWidth: 2, d: "M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" })));
-};
 
 const SelectContext = createContext({
     value: null,
@@ -357,9 +353,6 @@ const SearchInput = ({ placeholder = "", value = "", onChange, name = "" }) => {
     return (require$$0.createElement("div", { className: classNames && classNames.searchContainer
             ? classNames.searchContainer
             : "relative py-1 px-2.5" },
-        require$$0.createElement(SearchIcon, { className: classNames && classNames.searchIcon
-                ? classNames.searchIcon
-                : "absolute w-5 h-5 mt-2.5 pb-0.5 ml-2 text-gray-500" }),
         require$$0.createElement("input", { className: classNames && classNames.searchBox
                 ? classNames.searchBox
                 : "w-full py-2 pl-8 text-sm text-gray-500 bg-gray-100 border border-gray-200 rounded focus:border-gray-200 focus:ring-0 focus:outline-none", type: "text", placeholder: placeholder, value: value, onChange: onChange, name: name })));
